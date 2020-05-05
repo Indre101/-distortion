34a6fa1e0cf39afe08577ea97292ae636f25efeb
indre101 <zygaityte.indre@gmail.com>
2020-05-05 09:44:17 +0200

    gets the data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState([]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      getData(setCount);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return <div className="App">hi</div>;
 }
 
 export default App;
+
+const getData = async (setCount) => {
+  const data = await fetch("https://kea-alt-del.dk/kata-distortion/");
+  const response = await data.json();
+  setCount((prevCount) => [response, ...prevCount]);
+};
